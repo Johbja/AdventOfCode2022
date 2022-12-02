e0a334fd35aefb752d6431292a5a31c6367af486
Johannes Bjärehall <jojeskate@hotmail.com>
2022-12-02 08:36:02 +0100

    Solution Day2

--- Advent_Of_Code_2022/Days/Day2.cs
+++ Advent_Of_Code_2022/Days/Day2.cs
@@ -12,12 +12,63 @@ namespace Advent_Of_Code_2022.Days
 
         public override void CaculateAnswerPartOne()
         {
-            PrintAnswerPartOne("");
+            var result = input.Select(s => CalculateOutcomeScore(s.Replace(" ", "")) ).Sum();
+
+            PrintAnswerPartOne($"The score following the guide will be {result}");
         }
 
         public override void CaculateAnswerPartTwo()
         {
-            PrintAnswerPartTwo("");
+            var result = input.Select(s => CalculateNeededMove(s.Replace(" ", ""))).Sum();
+
+            PrintAnswerPartTwo($"The score following the guide will be {result}");
         }
+
+        //A rock = 1
+        //B Paper = 2 
+        //C sissors = 3
+
+        //win = 6
+        //draw = 3
+        //loss = 0
+
+        //X Rock
+        //Y Paper
+        //Z Sissors
+
+        //X = lose
+        //Y = draw
+        //Z = win
+
+        private int CalculateOutcomeScore(string moves)
+            => moves switch
+            {
+                "AX" => 3 + 1,
+                "AY" => 6 + 2,
+                "AZ" => 0 + 3,
+                "BX" => 0 + 1,
+                "BY" => 3 + 2,
+                "BZ" => 6 + 3,
+                "CX" => 6 + 1,
+                "CY" => 0 + 2,
+                "CZ" => 3 + 3,
+                _ => 0,
+            };
+
+        private int CalculateNeededMove(string moves) 
+            => moves switch
+            {
+                "AX" => 0 + 3,
+                "AY" => 3 + 1,
+                "AZ" => 6 + 2,
+                "BX" => 0 + 1,
+                "BY" => 3 + 2,
+                "BZ" => 6 + 3,
+                "CX" => 0 + 2,
+                "CY" => 3 + 3,
+                "CZ" => 6 + 1,
+                _ => 0,
+            };
+
     }
 }

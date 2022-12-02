b6649114e3aef6a2fe43cf6b63aae78e645d2751
Johannes Bjärehall <jojeskate@hotmail.com>
2022-12-02 17:26:15 +0100

    Optimised Day2

--- Advent_Of_Code_2022/Days/Day2.cs
+++ Advent_Of_Code_2022/Days/Day2.cs
@@ -8,17 +8,28 @@ namespace Advent_Of_Code_2022.Days
 {
     public class Day2 : Solution
     {
-        public Day2(string path, Type instanceType) : base(path, instanceType) {}
+        private int arrayLength = 3;
+        
+        private int[][] scoreTable =
+        {
+            new int[] { 3, 6, 0 },
+            new int[] { 0, 3, 6 },
+            new int[] { 6, 0, 3 }
+        };
+
+        public Day2(string path, Type instanceType) : base(path, instanceType) 
+        {
+            
+        }
+
+        private int ToIndex(char input, int offset) 
+            => ((int)input + offset) % arrayLength;
 
         public override void CaculateAnswerPartOne()
         {
             RunProtectedAction(() =>
             {
-                char t = 'B';
-                var n = (int)t;
-
-                var result = input.Select(s => CalculateOutcome(s)).Sum();
-
+                var result = input.Select(s => CalculateScore(s)).Sum();
                 PrintAnswerPartOne($"The score following the guide will be {result}");
             });
         }
@@ -27,59 +38,24 @@ namespace Advent_Of_Code_2022.Days
         {
             RunProtectedAction(() =>
             {
-                var result = input.Select(s => CalculateOutcomeWithNeededMove(s)).Sum();
-
+                var result = input.Select(s => CalculateScoreWithCondition(s)).Sum();
                 PrintAnswerPartTwo($"The score following the guide will be {result}");
             });
         }
 
-        //A rock = 1
-        //B Paper = 2 
-        //C sissors = 3
+        private int CalculateScore(string s)
+        {
+            var opponentMove = ToIndex(s[0], 1);
+            var move = ToIndex(s[2], -1);
+            return scoreTable[opponentMove][move] + move + 1;
+        }
 
-        //win = 6
-        //draw = 3
-        //loss = 0
-        
-        //part 1
-        //X Rock
-        //Y Paper
-        //Z Sissors
-
-        private int CalculateOutcome(string moves)
-            => moves switch
-            {
-                "A X" => 1 + 3,
-                "A Y" => 2 + 6,
-                "A Z" => 3 + 0,
-                "B X" => 1 + 0,
-                "B Y" => 2 + 3,
-                "B Z" => 3 + 6,
-                "C X" => 1 + 6,
-                "C Y" => 2 + 0,
-                "C Z" => 3 + 3,
-                _ => 0,
-            };
-
-        //part 2
-        //X lose
-        //Y draw
-        //Z win
-
-        private int CalculateOutcomeWithNeededMove(string moves) 
-            => moves switch
-            {
-                "A X" => 3 + 0,
-                "A Y" => 1 + 3,
-                "A Z" => 2 + 6,
-                "B X" => 1 + 0,
-                "B Y" => 2 + 3,
-                "B Z" => 3 + 6,
-                "C X" => 2 + 0,
-                "C Y" => 3 + 3,
-                "C Z" => 1 + 6,
-                _ => 0,
-            };
-
+        private int CalculateScoreWithCondition(string s)
+        {
+            var opponentMove = ToIndex(s[0], 1);
+            var offset = ToIndex(s[2], -2);
+            var move = (opponentMove + offset) % arrayLength;
+            return scoreTable[opponentMove][move] + move + 1;
+        }
     }
 }

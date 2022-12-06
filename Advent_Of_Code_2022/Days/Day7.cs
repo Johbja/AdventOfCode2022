b7f09f0408d31b00a86fb643cf9df00343c95dad
Johannes Bjärehall <johannes.bjarehall@cgi.com>
2022-12-06 16:12:40 +0100

    added prepp for day 7

---
+++ Advent_Of_Code_2022/Days/Day7.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_Of_Code_2022.Days
+{
+    public class Day7 : Solution
+    {
+        public Day7(string path, Type instanceType, bool render) : base(path, instanceType, render)
+        {
+        }
+
+        public override void SolvePartOne()
+        {
+            RunProtectedAction(() =>
+            {
+
+
+                StoreAnswerPartOne("");
+            });
+        }
+
+        public override void SolvePartTwo()
+        {
+            RunProtectedAction(() =>
+            {
+
+
+                StoreAnswerPartTwo("");
+            });
+        }
+
+    }
+}

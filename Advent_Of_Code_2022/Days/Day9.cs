ff1e905336c4d5caf3a97fc88f4c9ec6cc04b663
Johannes Bjärehall <jojeskate@hotmail.com>
2022-12-08 07:21:01 +0100

    Day 9 prep

---
+++ Advent_Of_Code_2022/Days/Day9.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_Of_Code_2022.Days
+{
+    public class Day9 : Solution
+    {
+        public Day9(string path, Type instanceType, bool render) : base(path, instanceType, render)
+        {
+        }
+
+        protected override void SolvePartOne()
+        {
+            RunProtectedAction(() =>
+            {
+
+                StoreAnswerPartOne("");
+            });
+        }
+
+        protected override void SolvePartTwo()
+        {
+            RunProtectedAction(() =>
+            {
+
+                StoreAnswerPartTwo("");
+            });
+        }
+
+    }
+}

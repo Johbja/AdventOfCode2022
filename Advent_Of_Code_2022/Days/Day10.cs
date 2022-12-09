20ca2e0d264ca1c22d2d01dd6716608244689e48
Johannes Bjärehall <jojeskate@hotmail.com>
2022-12-09 11:31:55 +0100

    day 10 prep

--- Advent_Of_Code_2022/Days/Day10.cs
+++ Advent_Of_Code_2022/Days/Day10.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace Advent_Of_Code_2022.Days
 {
-    internal class Day10
+    public class Day10 : Solution
     {
+        public Day10(string path, Type instanceType, bool render) : base(path, instanceType, render)
+        {
+        }
+
+        protected override void SolvePartOne()
+        {
+            RunProtectedAction(() =>
+            {
+
+                StoreAnswerPartOne("");
+            });
+        }
+
+        protected override void SolvePartTwo()
+        {
+            RunProtectedAction(() =>
+            {
+
+                StoreAnswerPartTwo("");
+            });
+        }
+
     }
 }

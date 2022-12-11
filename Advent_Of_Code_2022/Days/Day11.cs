6fcb68ca85e22b644d222e96994c0ae6c15d2230
Johannes Bjärehall <jojeskate@hotmail.com>
2022-12-11 09:01:19 +0100

    solved day 11

--- Advent_Of_Code_2022/Days/Day11.cs
+++ Advent_Of_Code_2022/Days/Day11.cs
@@ -1,12 +1,16 @@
-﻿using Advent_Of_Code_2022.Utility.Day10;
+﻿using Advent_Of_Code_2022.CustomAttributes;
+using Advent_Of_Code_2022.Utility.Day10;
+using Advent_Of_Code_2022.Utility.Day11;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Advent_Of_Code_2022.Days
 {
+    [DayInfo("11", "Monkey in the Middle")]
     public class Day11 : Solution
     {
         public Day11(string path, Type instanceType, bool render) : base(path, instanceType, render)
@@ -18,8 +22,8 @@ namespace Advent_Of_Code_2022.Days
         {
             RunProtectedAction(() =>
             {
-
-                StoreAnswerPartOne($"");
+                var result = SimulateRounds(manageWorry: x => x / 3);
+                StoreAnswerPartOne($"Level of monkey business after 20 rounds = {result}");
             });
         }
 
@@ -27,9 +31,59 @@ namespace Advent_Of_Code_2022.Days
         {
             RunProtectedAction(() =>
             {
-
-                StoreAnswerPartTwo("");
+                var result = SimulateRounds(rounds: 10000, manageWorry: x => x / 3);
+                StoreAnswerPartTwo($"Level of monkey business after 10000 rounds = {result}");
             });
+        }
+
+        private long SimulateRounds(Func<long, long> manageWorry, int rounds = 20)
+        {
+            var monkeys = CreateMonkeys(input.Where(x => x.Length > 0).Chunk(6).ToList());
+
+            if (manageWorry(3) == 1)
+            {
+                long lcm = 1;
+                foreach (var monkey in monkeys)
+                {
+                    lcm *= monkey.Divider;
+                }
+
+                manageWorry = x => x % lcm;
+            }
+
+            for (int round = 0; round < rounds; round++)
+            {
+                foreach (var monkey in monkeys)
+                {
+                    monkey.EvaluateItems(manageWorry);
+                }
+            }
+
+            return monkeys.OrderByDescending(x => x.InspectCount).Take(2).Aggregate(1l, (a, b) => a * b.InspectCount);
+        }
+
+        private List<Monkey> CreateMonkeys(List<string[]> monkeysAsText)
+        {
+            List<Monkey> monkeys = new();
+
+            foreach(var monkey in monkeysAsText)
+            {
+                var id = int.Parse(monkey[0].Replace(":", "").Split(" ")[1]);
+                var items = monkey[1].Split(":")[1].Split(",", StringSplitOptions.RemoveEmptyEntries).Select(item => long.Parse(item)).ToList();
+                
+                var operation = monkey[2].Split("=", StringSplitOptions.RemoveEmptyEntries)[1].Split(" ");
+                Func<long, long> op = operation[2] == "+" ? (x) => x + (long.TryParse(operation[3], out long value) ? value : x) 
+                                                          : (x) => x * (long.TryParse(operation[3], out long value) ? value : x);
+
+                var divider = int.Parse(monkey[3].Split(" ", StringSplitOptions.RemoveEmptyEntries)[3]);
+                var trueOp = int.Parse(monkey[4].Split(" ", StringSplitOptions.RemoveEmptyEntries)[5]);
+                var falseOp = int.Parse(monkey[5].Split(" ", StringSplitOptions.RemoveEmptyEntries)[5]);
+
+                var newMonkey = new Monkey(id, items, divider, op, x => x % divider == 0 ? trueOp : falseOp, x => monkeys.Find(monkey => monkey.Id == x));
+                monkeys.Add(newMonkey);
+            }
+
+            return monkeys;
         }
     }
 }
---
+++ Advent_Of_Code_2022/Utility/Day11/Monkey.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_Of_Code_2022.Utility.Day11
+{
+    public class Monkey
+    {
+        public int Id { get; private set; }
+        public long InspectCount { get; private set; }
+        public long Divider { get; private set; }
+        public Queue<long> items { get; private set; }
+        
+        private Func<long, long> operation;
+        private Func<long, int> test;
+        private Func<int, Monkey?> aim;
+
+        public Monkey(int id, List<long> items, long divider, Func<long, long> operation, Func<long, int> test, Func<int, Monkey?> aim)
+        {
+            Id = id;
+            InspectCount = 0;
+            Divider = divider;
+            this.items = new();
+            items.ForEach(item => this.items.Enqueue(item));
+            this.operation = operation;
+            this.test = test;
+            this.aim = aim;
+        }
+
+        public void EvaluateItems(Func<long, long> manageWorry)
+        {
+            while (items.Any())
+            {
+                var item = items.Dequeue();
+                item = operation(item);
+
+                item = manageWorry(item);
+
+                var id = test(item);
+                var monkey = aim(id);
+
+                if (monkey is not null)
+                    monkey.CatchItem(item);
+
+                InspectCount++;
+            }
+        }
+
+        public void CatchItem(long item)
+        {
+            items.Enqueue(item);
+        }
+
+    }
+}

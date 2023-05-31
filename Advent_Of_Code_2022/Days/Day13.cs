f2cd15ef59065e352a2669df004564721d595b66
Johannes Bjärehall <jojeskate@hotmail.com>
2023-06-01 01:11:44 +0200

    solved day 13 part 1

--- Advent_Of_Code_2022/Days/Day13.cs
+++ Advent_Of_Code_2022/Days/Day13.cs
@@ -6,58 +6,19 @@ namespace Advent_Of_Code_2022.Days
     [DayInfo("13", "description")]
     public class Day13 : Solution
     {
-        public Day13(string path, Type instanceType, bool render) 
+        private readonly List<string[]> pairs;
+
+        public Day13(string path, Type instanceType, bool render)
             : base(path, instanceType, render)
         {
-            
+            pairs = input.Where(s => s.Length > 0).Chunk(2).ToList();
         }
 
         protected override void SolvePartOne()
         {
-            /*
-                If both values are integers, 
-                    left < right = correct  left == right = continue 
-                If both values are lists, 
-                    Index compare with step 1
-                    right.count < left.count = wrong
-                If exactly one value is an integer, 
-                    convert the integer to a list and do step 2
-
-
-                [1,1,3,1,1]
-                [1,1,5,1,1]
-                
-                [[1],[2,3,4]]
-                [[1],4]
-                
-                [9]
-                [[8,7,6]]
-                
-                [[4,4],4,4]
-                [[4,4],4,4,4]
-                
-                [7,7,7,7]
-                [7,7,7]
-                
-                []
-                [3]
-                
-                [[[]]]
-                [[]]
-                
-                [1,[2,[3,[4,[5,6,7]]]],8,9]
-                [1,[2,[3,[4,[5,6,0]]]],8,9]
-
-             */
-
-
             RunProtectedAction(() =>
             {
-                var data = input.Where(s => s.Length > 0).Chunk(2);
-
-                var res = data.Select((pair, i)=> (pair: pair.Select(s => GetSubListIndecies(s)).ToArray(), index: i + 1)).ToList();
-                var sum = res.Where(x => ComparePair(x.pair[0], x.pair[1])).Sum(x => x.index);
-
+                var sum = GetIndecies().Sum();
                 StoreAnswerPartOne($"sum of all correct indecies = {sum}");
             });
         }
@@ -66,67 +27,14 @@ namespace Advent_Of_Code_2022.Days
         {
             RunProtectedAction(() =>
             {
-
+                var indecies = GetIndecies();
                 StoreAnswerPartTwo("");
             });
         }
 
-        private List<Node> GetSubListIndecies(string data)
-        {
-            List<Node> nodes = new();
-            Stack<Node> stack = new();
-            Node current = null;
-
-            foreach (char c in data)
-            {
-                if (c == '[')
-                {
-                    Node newNode = new Node();
-                    if (current != null)
-                    {
-                        current.Children.Add(newNode);
-                    }
-                    stack.Push(newNode);
-                    current = newNode;
-                }
-                else if (c == ']')
-                {
-                    if (stack.Count > 1)
-                    {
-                        stack.Pop();
-                        current = stack.Peek();
-                    }
-                }
-                else if (char.IsDigit(c))
-                {
-                    int value = int.Parse(c.ToString());
-                    Node newNode = new Node { Value = value };
-                    if (current != null)
-                    {
-                        current.Children.Add(newNode);
-                    }
-                }
-            }
-
-            return nodes;
-        }
-
-        private bool ComparePair(List<Node> nodes1, List<Node> nodes2)
-        {
-            if (nodes1.Count != nodes2.Count)
-            {
-                return false;
-            }
-
-            for (int i = 0; i < nodes1.Count; i++)
-            {
-                if (!nodes1[i].Compare(nodes2[i]))
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
+        private IEnumerable<int> GetIndecies()
+            => pairs.Select((pair, i) => (value: Packet.CompareLists(Parser.Parse(pair[0].Trim()), Parser.Parse(pair[1].Trim())), index: i + 1))
+                .Where(x => x.value <= 0)
+                .Select(x => x.index);
     }
 }
---
+++ Advent_Of_Code_2022/Utility/Day13/Parser.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_Of_Code_2022.Utility.Day13;
+
+public static class Parser
+{
+    public static Queue<char> StringToQueue(string toParse)
+    {
+        Queue<char> queue = new Queue<char>();
+        foreach (char ch in toParse)
+        {
+            queue.Enqueue(ch);
+        }
+        return queue;
+    }
+
+    public static List<object> Parse(string toParse)
+    {
+        Queue<char> data = StringToQueue(toParse);
+        List<object> list = ParseList(data);
+        return list;
+    }
+
+    public static List<object> ParseList(Queue<char> data)
+    {
+        List<object> elements = new();
+        data.Dequeue(); // Remove the '[' from the queue
+        while (data.Peek() != ']')
+        {
+            if (data.Peek() == ',')
+            {
+                data.Dequeue();
+            }
+            object el = ParseElement(data);
+            elements.Add(el);
+        }
+        data.Dequeue(); // Remove the ']' from the queue
+        return elements;
+    }
+
+    public static object ParseElement(Queue<char> data)
+    {
+        char next = data.Peek();
+        if (char.IsDigit(next))
+        {
+            return ParseInt(data);
+        }
+        else if (next == '[')
+        {
+            return ParseList(data);
+        }
+        else
+        {
+            throw new Exception($"Expected an int or list but found: {string.Join("", data)}");
+        }
+    }
+
+    public static int ParseInt(Queue<char> data)
+    {
+        string token = string.Empty;
+        while (char.IsDigit(data.Peek()))
+        {
+            token += data.Dequeue();
+        }
+        return int.Parse(token);
+    }
+
+}

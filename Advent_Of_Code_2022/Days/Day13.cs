841320741141182c4c9bb233206407503cb20fbb
Johannes Bjärehall <johannes.bjarehall@cgi.com>
2023-05-31 20:54:47 +0200

    changes to day 13

--- Advent_Of_Code_2022/Days/Day13.cs
+++ Advent_Of_Code_2022/Days/Day13.cs
@@ -1,39 +1,62 @@
 ﻿using Advent_Of_Code_2022.CustomAttributes;
-using Advent_Of_Code_2022.Extensions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Advent_Of_Code_2022.Utility.Day13;
 
 namespace Advent_Of_Code_2022.Days
 {
     [DayInfo("13", "description")]
     public class Day13 : Solution
     {
-        public Day13(string path, Type instanceType, bool render) : base(path, instanceType, render)
+        public Day13(string path, Type instanceType, bool render) 
+            : base(path, instanceType, render)
         {
             
         }
 
         protected override void SolvePartOne()
         {
+            /*
+                If both values are integers, 
+                    left < right = correct  left == right = continue 
+                If both values are lists, 
+                    Index compare with step 1
+                    right.count < left.count = wrong
+                If exactly one value is an integer, 
+                    convert the integer to a list and do step 2
+
+
+                [1,1,3,1,1]
+                [1,1,5,1,1]
+                
+                [[1],[2,3,4]]
+                [[1],4]
+                
+                [9]
+                [[8,7,6]]
+                
+                [[4,4],4,4]
+                [[4,4],4,4,4]
+                
+                [7,7,7,7]
+                [7,7,7]
+                
+                []
+                [3]
+                
+                [[[]]]
+                [[]]
+                
+                [1,[2,[3,[4,[5,6,7]]]],8,9]
+                [1,[2,[3,[4,[5,6,0]]]],8,9]
+
+             */
+
+
             RunProtectedAction(() =>
             {
-                var pairs = input.Where(s => s.Length > 0).Chunk(2).Select(p => p.Select(s => SubDivideInput(s)).ToArray()).ToList();
+                var data = input.Where(s => s.Length > 0).Chunk(2);
 
-                int sum = 0;
-                for(int pair = 0; pair < pairs.Count; pair++)
-                {
-                    var result = IsCorrect(pairs[pair][0], pairs[pair][1]);
-                    
-                    if (result.HasValue && result.Value == false)
-                    {
-                        continue;
-                    }
-                    
-                    sum += pair + 1;
-                }
+                var res = data.Select((pair, i)=> (pair: pair.Select(s => GetSubListIndecies(s)).ToArray(), index: i + 1)).ToList();
+                var sum = res.Where(x => ComparePair(x.pair[0], x.pair[1])).Sum(x => x.index);
 
                 StoreAnswerPartOne($"sum of all correct indecies = {sum}");
             });
@@ -48,122 +71,62 @@ namespace Advent_Of_Code_2022.Days
             });
         }
 
-        private bool? IsCorrect(Packet pLeft, Packet pRight)
+        private List<Node> GetSubListIndecies(string data)
         {
-            
-            if(!pLeft.HasValue && !pRight.HasValue)
+            List<Node> nodes = new();
+            Stack<Node> stack = new();
+            Node current = null;
+
+            foreach (char c in data)
             {
-                for(int i = 0; i < pLeft.Packets.Count; i++)
+                if (c == '[')
                 {
-                    //right runs out of items first, not correct
-                    if (i >= pRight.Packets.Count)
-                        return false;
-
-                    var check = IsCorrect(pLeft.Packets[i], pRight.Packets[i]);
-                    
-                    if (check is not null)
-                        return check;
-                }
-            }
-
-            if(pLeft.HasValue && !pRight.HasValue)
-            {
-                var Converted = new List<Packet>() { pLeft };
-                var ConvertetPLeft = new Packet(packets: Converted);
-                return IsCorrect(ConvertetPLeft, pRight);
-            }
-
-            if (!pLeft.HasValue && pRight.HasValue)
-            {
-                var Converted = new List<Packet>() { pRight };
-                var ConvertetPRight = new Packet(packets: Converted);
-                return IsCorrect(pLeft, ConvertetPRight);
-            }
-
-            if (pLeft.HasValue && pRight.HasValue)
-            {
-                if (pLeft.Value < pRight.Value)
-                    return true;
-
-                if (pLeft.Value > pRight.Value)
-                    return false;
-            }
-
-            return null;
-        }
-
-        private Packet? SubDivideInput(string part)
-        {
-            part = part.Substring(1, part.Length - 2);
-            Stack<Queue<Packet>> packetStack = new();
-            Queue<Packet> currentPackets = new Queue<Packet>();
-
-
-            for (int i = 0; i < part.Length; i++)
-            {
-                if (part[i] == '[')
-                {
-                    packetStack.Push(currentPackets);
-                    currentPackets = new Queue<Packet>();
-                    continue;
-                }
-
-                if (int.TryParse(part[i].ToString(), out int value))
-                {
-                    var p = new Packet(part: value);
-                    currentPackets.Enqueue(p);
-                }
-
-                if (part[i] == ']')
-                {
-                    
-                    List<Packet> packets = new();
-                    while (currentPackets.Any())
+                    Node newNode = new Node();
+                    if (current != null)
                     {
-                        packets.Add(currentPackets.Dequeue());
+                        current.Children.Add(newNode);
                     }
-                    var packet = new Packet(packets: packets);
-                    
-                    currentPackets = packetStack.Pop();
-                    currentPackets.Enqueue(packet);
+                    stack.Push(newNode);
+                    current = newNode;
+                }
+                else if (c == ']')
+                {
+                    if (stack.Count > 1)
+                    {
+                        stack.Pop();
+                        current = stack.Peek();
+                    }
+                }
+                else if (char.IsDigit(c))
+                {
+                    int value = int.Parse(c.ToString());
+                    Node newNode = new Node { Value = value };
+                    if (current != null)
+                    {
+                        current.Children.Add(newNode);
+                    }
                 }
             }
 
-            List<Packet> subLists = new();
-            while (currentPackets.Any())
-            {
-                subLists.Add(currentPackets.Dequeue());
-            }
-
-            var newPacket = new Packet(packets: subLists);
-
-            return newPacket;
-        }
-    }
-
-    public class Packet
-    {
-        public int Value;
-        public List<Packet> Packets;
-
-        public bool HasValue
-        {
-            get
-            {
-                return Packets is null ? true : false;
-            }
+            return nodes;
         }
 
-        public Packet(int? part = null, List<Packet> packets = null)
+        private bool ComparePair(List<Node> nodes1, List<Node> nodes2)
         {
-            if(part.HasValue)
+            if (nodes1.Count != nodes2.Count)
             {
-                Value = part.Value;
+                return false;
             }
-            else
+
+            for (int i = 0; i < nodes1.Count; i++)
             {
-                Packets = packets;
+                if (!nodes1[i].Compare(nodes2[i]))
+                {
+                    return false;
+                }
             }
+
+            return true;
         }
     }
 }

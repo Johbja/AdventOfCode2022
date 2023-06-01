1e4392a18de02dde8b153bd4218340339112c1d0
Johannes Bjärehall <jojeskate@hotmail.com>
2023-06-01 12:25:32 +0200

    solved day 13

--- Advent_Of_Code_2022/Days/Day13.cs
+++ Advent_Of_Code_2022/Days/Day13.cs
@@ -27,8 +27,24 @@ namespace Advent_Of_Code_2022.Days
         {
             RunProtectedAction(() =>
             {
-                var indecies = GetIndecies();
-                StoreAnswerPartTwo("");
+                var data = input.Where(s => s.Length > 0).ToList();
+
+                int twoCounter = 0;
+                int sixCounter = 0;
+                var twoPacket = Parser.Parse("[[2]]");
+                var sixPacket = Parser.Parse("[[6]]");
+                foreach (var packet in data)
+                {
+                    if (Packet.CompareLists(twoPacket, Parser.Parse(packet.Trim())) > 0)
+                        twoCounter++;
+                    if (Packet.CompareLists(sixPacket, Parser.Parse(packet.Trim())) > 0)
+                        sixCounter++;
+                }
+
+                int posOf2 = twoCounter + 1;
+                int posOf6 = sixCounter + 2;
+
+                StoreAnswerPartTwo($"product of positions is {posOf2 * posOf6}");
             });
         }
 
@@ -38,3 +54,50 @@ namespace Advent_Of_Code_2022.Days
                 .Select(x => x.index);
     }
 }
+
+
+/*
+[1,1,3,1,1]
+[1,1,5,1,1]                 is
+
+[[1],[2,3,4]]
+[[1],4]                     is
+
+[9]
+[[8,7,6]]                   not
+
+[[4,4],4,4]
+[[4,4],4,4,4]               is
+
+[7,7,7,7]
+[7,7,7]                     not
+
+[]
+[3]                         is
+
+[[[]]]
+[[]]                    not
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9] not
+ 
+[]
+[[]]
+[[[]]]
+[1,1,3,1,1]
+[1,1,5,1,1]
+[[1],[2,3,4]]
+[1,[2,[3,[4,[5,6,0]]]],8,9]
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[[1],4]
+[[2]]
+[3]
+[[4,4],4,4]
+[[4,4],4,4,4]
+[[6]]
+[7,7,7]
+[7,7,7,7]
+[[8,7,6]]
+[9]
+ 
+ */
